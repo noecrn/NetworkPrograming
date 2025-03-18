b48f837a69d46d55b65c0fd96283c819054a5ddc
noe.cornu <noe.cornu1@gmail.com>
2025-03-18 16:22:45 +0700

    [ADD] Enhance client-server communication by adding input validation and calculation logic

--- Challenge_02/Client/client-select.py
+++ Challenge_02/Client/client-select.py
@@ -1,21 +1,28 @@
 import socket
 import sys
+import re
 
-server_address = ('127.0.0.1' , 5000)
+server_address = ('127.0.0.1', 5000)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
-
+print("Enter mathematical operations (e.g., 3*5, 7/2, 6-4)")
 sys.stdout.write('>> ')
 
-
 try:
-	while True:
-		message = str(input())
-		client_socket.send (message.encode())
-		sys.stdout.write(client_socket.recv(1024) .decode())
-		sys.stdout.write('>> ')
+    while True:
+        message = str(input())
+        # Validate input format using regex
+        if not re.match(r'^\d+[\+\-\*\/]\d+$', message):
+            print("Invalid format! Use format: number operator number (e.g., 3*5)")
+            sys.stdout.write('>> ')
+            continue
+            
+        client_socket.send(message.encode())
+        response = client_socket.recv(1024).decode()
+        print(response)
+        sys.stdout.write('>> ')
 
 except KeyboardInterrupt:
-	client_socket.close()
-	sys.exit(0)
+    client_socket.close()
+    sys.exit(0)
