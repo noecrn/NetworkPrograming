86df7e2910cb260e121b2ed14f8185d6d20d2d62
noe.cornu <noe.cornu1@gmail.com>
2025-04-08 16:10:26 +0700

    [UPDATE] Enhance client-server communication with prime number checking and improved message handling

--- Challenge7_Group08/client.py
+++ Challenge7_Group08/client.py
@@ -10,16 +10,26 @@ server.connect((ip_address, port))
 
 def send_msg(sock):
     while True:
-        data = sys.stdin.readline()
-        sock.send(data.encode())
-        sys.stdout.write('<You>')
-        sys.stdout.write(data)
+        sys.stdout.write(">> ")
         sys.stdout.flush()
+        data = sys.stdin.readline().strip()
+        if data:
+            sock.send(data.encode())
+            if not data.startswith('prime'):
+                print(f"You: {data}")
 
 def recv_msg(sock):
     while True:
-        data = sock.recv(2048)
-        sys.stdout.write(data.decode())
+        try:
+            data = sock.recv(2048).decode()
+            if data:
+                sys.stdout.write(data)
+                sys.stdout.write(">> ")
+                sys.stdout.flush()
+        except:
+            print("Lost connection to server")
+            sock.close()
+            sys.exit()
 
 Thread(target=send_msg, args=(server,)).start()
 Thread(target=recv_msg, args=(server,)).start()
--- Challenge7_Group08/server.py
+++ Challenge7_Group08/server.py
@@ -2,6 +2,7 @@ import socket
 import select
 import sys
 import threading
+import re
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -10,19 +11,46 @@ port = 8081
 server.bind((ip_address, port))
 server.listen(100)
 list_of_clients = []
+client_letters = {}  # Dictionary to map connections to letters
+next_letter = ord('A')  # Start with 'A'
+
+def is_prime(n):
+    try:
+        num = int(n)
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+    except ValueError:
+        return False
 
 def clientthread(conn, addr):
+    client_letter = client_letters[conn]
     while True:
         try:
             message = conn.recv(2048).decode()
             if message:
-                print('<' + addr[0] + '>' + message)
-                message_to_send = '<' + addr[0] + '>' + message
-                broadcast(message_to_send, conn)
+                # Check for prime number command
+                prime_match = re.match(r'prime\s+(\d+)', message.strip())
+                if prime_match:
+                    number = prime_match.group(1)
+                    result = is_prime(number)
+                    response = f"{number} is{' ' if result else ' not '}a prime number"
+                    print(f"On Server: {response}")
+                    message_to_send = f"Client {client_letter}: {response}\n"
+                    broadcast(message_to_send, conn)
+                else:
+                    print(f"Client {client_letter}: {message}")
+                    message_to_send = f"Client {client_letter}: {message}\n"
+                    broadcast(message_to_send, conn)
             else:
                 remove(conn)
+                break
         except:
-            continue
+            remove(conn)
+            break
 
 def broadcast(message, connection):
     for clients in list_of_clients:
@@ -36,11 +64,20 @@ def broadcast(message, connection):
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
+        if connection in client_letters:
+            del client_letters[connection]
+
+def assign_next_letter():
+    global next_letter
+    letter = chr(next_letter)
+    next_letter = ord('A') if next_letter >= ord('Z') else next_letter + 1
+    return letter
 
 while True:
     conn, addr = server.accept()
     list_of_clients.append(conn)
-    print(addr[0] + ' connected')
+    client_letters[conn] = assign_next_letter()
+    print(f"Client {client_letters[conn]} connected")
     threading.Thread(target=clientthread, args=(conn, addr)).start()
 
 conn.close()
